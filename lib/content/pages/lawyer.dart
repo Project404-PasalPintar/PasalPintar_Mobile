5604ca24a434be52d3d7b2027f58e0af4fc4d4f5
AldiMusneldi <aldimusneldi@gmail.com>
2024-12-14 15:52:58 +0700

    feat(home): slicing ui home page

---
+++ lib/content/pages/lawyer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Lawyer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Messages Page',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
